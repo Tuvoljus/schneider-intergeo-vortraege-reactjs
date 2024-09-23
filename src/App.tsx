3f74feed82de0cc00efbd962fdbb9ef7c8cd9258
Alexander <alexander.keil1@web.de>
2024-09-23 22:33:42 +0200

    Accordion style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import './scss/main.scss'
 import React from 'react';
-import logo from './logo.svg';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// import logo from './logo.svg';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-import 'swiper/css';
+// import 'swiper/css';
 import './App.css';
 import { ScheduleTable } from './Table_Schedule';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { Badge } from 'react-bootstrap';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+// import { Badge } from 'react-bootstrap';
 
 
 function App() {
